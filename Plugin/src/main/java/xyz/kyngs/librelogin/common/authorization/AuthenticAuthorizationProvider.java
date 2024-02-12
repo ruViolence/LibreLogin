fe2673f8cf55eb42453de589012e5862b221b394
ruViolence <78062896+ruViolence@users.noreply.github.com>
2024-02-12 14:33:05 +0800

    Remove unused

--- Plugin/src/main/java/xyz/kyngs/librelogin/common/authorization/AuthenticAuthorizationProvider.java
+++ Plugin/src/main/java/xyz/kyngs/librelogin/common/authorization/AuthenticAuthorizationProvider.java
@@ -6,8 +6,6 @@
 
 package xyz.kyngs.librelogin.common.authorization;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
@@ -26,13 +24,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.TimeUnit;
 
 public class AuthenticAuthorizationProvider<P, S> extends AuthenticHandler<P, S> implements AuthorizationProvider<P> {
 
     private final Map<P, Boolean> unAuthorized;
-    private final Cache<UUID, EmailVerifyData> emailConfirmCache;
-    private final Cache<UUID, String> passwordResetCache;
 
     public AuthenticAuthorizationProvider(AuthenticLibreLogin<P, S> plugin) {
         super(plugin);
@@ -45,28 +40,10 @@ public class AuthenticAuthorizationProvider<P, S> extends AuthenticHandler<P, S>
         }
 
         plugin.repeat(this::broadcastActionbars, 0, 1000);
-
-        emailConfirmCache = Caffeine.newBuilder()
-                .expireAfterWrite(10, TimeUnit.MINUTES)
-                .build();
-
-        passwordResetCache = Caffeine.newBuilder()
-                .expireAfterWrite(10, TimeUnit.MINUTES)
-                .build();
-    }
-
-    public Cache<UUID, EmailVerifyData> getEmailConfirmCache() {
-        return emailConfirmCache;
-    }
-
-    public Cache<UUID, String> getPasswordResetCache() {
-        return passwordResetCache;
     }
 
     public void onExit(P player) {
         stopTracking(player);
-        emailConfirmCache.invalidate(platformHandle.getUUIDForPlayer(player));
-        passwordResetCache.invalidate(platformHandle.getUUIDForPlayer(player));
     }
 
     @Override
@@ -174,8 +151,5 @@ public class AuthenticAuthorizationProvider<P, S> extends AuthenticHandler<P, S>
         });
 
         wrong.forEach(unAuthorized::remove);
-    }
-
-    public record EmailVerifyData(String email, String token, UUID uuid) {
     }
 }
